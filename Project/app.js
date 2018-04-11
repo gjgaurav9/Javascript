8e855bafa39c40a95940ece5f729067d4f2b5732
gjgaurav9 <gauravjainjavarnd@gmail.com>
2018-04-11 16:30:11 -0400

    Javascript first commit

---
+++ Project/app.js
@@ -0,0 +1,24 @@
+console.log('Hello Worldssss');
+console.log(123);
+console.log(true);
+var hello='Hello hi bye bye';
+console.log(hello);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1,b:2});
+
+console.error('This is an error');
+console.warn('This is warning');
+
+console.time('hello');
+
+console.log('Hello Worldssss');
+console.log('Hello Worldssss');
+console.log('Hello Worldssss');
+console.log('Hello Worldssss');
+console.log('Hello Worldssss');
+console.log('Hello Worldssss');
+console.log('Hello Worldssss');
+
+
+console.timeEnd('hell');
