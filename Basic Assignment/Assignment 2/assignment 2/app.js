204587e7ff1e2078662d51c2517b8844bb87845c
gjgaurav9 <gauravjainjavarnd@gmail.com>
2018-04-19 18:07:33 -0400

    assignment progress

---
+++ Basic Assignment/Assignment 2/assignment 2/app.js
@@ -0,0 +1,18 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var date = today.getDate();
+    var month = today.getMonth();
+    var year = today.getFullYear();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m +" "+ month+"/"+date+"/"+year;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
