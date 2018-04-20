8282f293d6a3fdca4352815648cdc9884c87877a
gjgaurav9 <gauravjainjavarnd@gmail.com>
2018-04-20 18:14:17 -0400

    Upated till imp links

--- Basic Assignment/Assignment 2/assignment 2/app.js
+++ Basic Assignment/Assignment 2/assignment 2/app.js
@@ -6,12 +6,14 @@ function startTime() {
     var date = today.getDate();
     var month = today.getMonth();
     var year = today.getFullYear();
+    
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
     h + ":" + m +" "+ month+"/"+date+"/"+year;
     var t = setTimeout(startTime, 500);
 }
+
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
--- Notes/DOM/11 Local  Session Storage.js
+++ Notes/DOM/11 Local  Session Storage.js
@@ -17,7 +17,7 @@
 
 // console.log(name, age);
 
-document.querySelector('form').addEventListener('submit', function(e){
+ document.querySelector('form').addEventListener('submit', function(e){
   const task = document.getElementById('task').value;
 
   let tasks;
